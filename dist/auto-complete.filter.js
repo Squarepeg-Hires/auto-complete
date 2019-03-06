ee4340e8841a9ba7aaa1455f8a4043de269c3574
Dan Pupaza <dpupaza@gmail.com>
2019-03-06 12:28:15 -0500

    added build and dist

---
+++ dist/auto-complete.filter.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AutoCompleteFilter = /** @class */ (function () {
+    function AutoCompleteFilter() {
+    }
+    return AutoCompleteFilter;
+}());
+exports.AutoCompleteFilter = AutoCompleteFilter;
+//# sourceMappingURL=auto-complete.filter.js.map
